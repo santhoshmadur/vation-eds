f4245834295ac7d352cc701573066076c4ccfce4
santhoshmadur <santhosh.madur@vation.com>
2025-07-16 11:08:59 +0530

    updated logo js

---
+++ blocks/logo/logo.js
@@ -0,0 +1,15 @@
+export default function decorate(block) {
+  const link = block.querySelector('div a')?.getAttribute('href') || '#';
+  const img = block.querySelector('picture img');
+  const src = img?.getAttribute('src') || '';
+  const altText = img?.getAttribute('alt')?.trim() || 'logo';
+
+  const htmlLiteral = `
+    <span class="logo">  
+      <a class="logo__picture" href="${link}">
+        <img src="${src}" alt="${altText}">
+      </a>
+    </span>
+  `;
+  block.innerHTML = utility.sanitizeHtml(htmlLiteral);
+}
