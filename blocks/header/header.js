404bf669772627723009443cbbb516e449029c99
santhoshmadur <santhosh.madur@vation.com>
2025-07-17 11:50:47 +0530

    update

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,3 +1,20 @@
+import { getMetadata } from '../../scripts/aem.js';
+import { loadFragment } from '../fragment/fragment.js';
+
+/**
+ * loads and decorates the header
+ * @param {Element} block The header block element
+ */
 export default async function decorate(block) {
-  const logo = block.querySelector('logo-wrapper header-footer-logo img');
+  // load header as fragment
+  const headerMeta = getMetadata('header');
+  const headerPath = headerMeta ? new URL(headerMeta, window.location).pathname : '/nav';
+  const fragment = await loadFragment(headerPath);
+
+  // decorate header DOM
+  block.textContent = '';
+  const header = document.createElement('div');
+  while (fragment.firstElementChild) header.append(fragment.firstElementChild);
+
+  block.append(header);
 }
