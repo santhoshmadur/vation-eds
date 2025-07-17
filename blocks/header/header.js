b100c899bdbf3f545025e302da629ac2cf2de27a
santhoshmadur <santhosh.madur@vation.com>
2025-07-17 11:23:03 +0530

    updated

---
+++ blocks/header/header.js
@@ -0,0 +1,14 @@
+import { getMetadata } from '../../scripts/aem.js';
+import { loadFragment } from '../fragment/fragment.js';
+
+export default async function decorate(block) {
+  // load nav as fragment
+  const navMeta = getMetadata('nav');
+  const navPath = navMeta ? new URL(navMeta, window.location).pathname : '/nav';
+  const fragment = await loadFragment(navPath);
+
+  // decorate nav DOM
+  block.textContent = '';
+  const nav = document.createElement('nav');
+    nav.innerHTML = fragment;
+}
