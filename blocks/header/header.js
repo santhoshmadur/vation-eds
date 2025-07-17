13bfdb35a18e9e6cdb083f3c9598b8e168b92f2e
santhoshmadur <santhosh.madur@vation.com>
2025-07-17 11:30:04 +0530

    updated

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,12 +1,3 @@
-import { getMetadata } from '../../scripts/aem.js';
-import { loadFragment } from '../fragment/fragment.js';
-
 export default async function decorate(block) {
-  // load nav as fragment
-  const navMeta = getMetadata('nav');
-  const navPath = navMeta ? new URL(navMeta, window.location).pathname : '/nav';
-
-  // decorate nav DOM
-  block.textContent = '';
-  const nav = document.createElement('nav');
+  const logo = block.querySelector('logo-wrapper header-footer-logo img');
 }
