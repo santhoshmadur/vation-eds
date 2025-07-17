19ff7eac9c1c37bdc8ef6c2ad4e4cc23738669a0
santhoshmadur <santhosh.madur@vation.com>
2025-07-17 11:25:47 +0530

    updated

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -5,10 +5,8 @@ export default async function decorate(block) {
   // load nav as fragment
   const navMeta = getMetadata('nav');
   const navPath = navMeta ? new URL(navMeta, window.location).pathname : '/nav';
-  const fragment = await loadFragment(navPath);
 
   // decorate nav DOM
   block.textContent = '';
   const nav = document.createElement('nav');
-    nav.innerHTML = fragment;
 }
