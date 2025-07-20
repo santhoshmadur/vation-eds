a2058dbe2575a762b4c2dd90436a71e5298ca7d9
santhoshmadur <santhosh.madur@vation.com>
2025-07-20 17:17:25 +0530

    updated

--- blocks/logo/logo.js
+++ blocks/logo/logo.js
@@ -1,15 +1,26 @@
 export default function decorate(block) {
-  const link = block.querySelector('div a')?.getAttribute('href') || '';
+  const link = block.querySelector('div a')?.getAttribute('href');
   const img = block.querySelector('picture img');
   const src = img?.getAttribute('src') || '';
   const altText = img?.getAttribute('alt')?.trim() || 'logo';
 
-  const htmlLiteral = `
-    <span class="logo">  
-      <a class="logo-picture" href="${link}">
+  let htmlLiteral = '';
+  if (link && link.trim() !== '') {
+    htmlLiteral = `
+      <span class="logo">  
+        <a class="logo-picture" href="${link}">
+          <img src="${src}" alt="${altText}">
+        </a>
+      </span>
+    `;
+  } else {
+    htmlLiteral = `
+      <span class="logo">
+      <div class="logo-picture">
         <img src="${src}" alt="${altText}">
-      </a>
-    </span>
-  `;
+        </div>
+      </span>
+    `;
+  }
   block.innerHTML = htmlLiteral;
 }
