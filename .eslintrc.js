6e08642286b8f7ce6ba76f1e656d4ef474ba1e34
santhoshmadur <santhosh.madur@vation.com>
2025-07-22 15:52:23 +0530

    updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,5 +18,11 @@ module.exports = {
     'import/extensions': ['error', { js: 'always' }], // require js file extensions in imports
     'linebreak-style': ['error', 'unix'], // enforce unix linebreaks
     'no-param-reassign': [2, { props: false }], // allow modifying properties of param
+    'xwalk/max-cells': [
+      'error',
+      {
+        video: 5, // update this value based on your needs
+      },
+    ],
   },
 };
