726b78f3bcd322aa1bfe2ff456b68a8a6a49959c
santhoshmadur <santhosh.madur@vation.com>
2026-01-28 11:42:58 +0530

    updated with test block

--- blocks/test/test.js
+++ blocks/test/test.js
@@ -1,7 +1,11 @@
 export default function decorate(block) {
   const url = 'https://edge-sandbox-graph.adobe.io/api/f59543c5-4f81-4114-b07f-cafe8c3df8b1/graphql?query=%7B%0A++getProduct%28id%3A+%2201tf6000002tDujAAE%22%29+%0A%7D';
 
-  block.innerHTML = '<pre>Loading...</pre>';
+  // Initial content
+  block.innerHTML = `
+    <div>testing api response</div>
+    <pre>Loading...</pre>
+  `;
 
   fetch(url)
     .then((response) => response.json())
@@ -9,11 +13,14 @@ export default function decorate(block) {
       const pre = document.createElement('pre');
       pre.textContent = JSON.stringify(data, null, 2);
 
-      block.innerHTML = '';
+      block.innerHTML = '<div>testing api response</div>';
       block.appendChild(pre);
     })
     .catch((error) => {
       console.error('Error:', error);
-      block.innerHTML = '<pre>Failed to load data</pre>';
+      block.innerHTML = `
+        <div>testing api response</div>
+        <pre>Failed to load data</pre>
+      `;
     });
 }
