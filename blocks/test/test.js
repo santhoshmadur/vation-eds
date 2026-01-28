cf539b88501d65845b94d3d4813893961c41889b
santhoshmadur <santhosh.madur@vation.com>
2026-01-28 11:48:01 +0530

    updated test bolck

--- blocks/test/test.js
+++ blocks/test/test.js
@@ -1,7 +1,6 @@
 export default function decorate(block) {
   const url = 'https://edge-sandbox-graph.adobe.io/api/f59543c5-4f81-4114-b07f-cafe8c3df8b1/graphql?query=%7B%0A++getProduct%28id%3A+%2201tf6000002tDujAAE%22%29+%0A%7D';
 
-  // Initial content
   block.innerHTML = `
     <div>testing api response</div>
     <pre>Loading...</pre>
